90dde0311ced0ca835850c271070b7072ae3f922
Gabriel <gabriel.chu.v@hotmail.com>
2022-02-01 20:08:32 +0000

    Create Chat component and set client side socket to communicate with backend

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import io from 'socket.io-client'; 
+
+let socket;
+
+function Chat() {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const location = useLocation();
+    const ENDPOINT = 'localhost:5000';
+    
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        
+        socket.emit('join', { name, room }, () => {
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+
+    },[ENDPOINT, location.search])
+
+    return (
+        <div>
+           <h1>Chat</h1> 
+        </div>
+    )
+}
+
+export default Chat
