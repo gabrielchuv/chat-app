d10b961644781ef2beee9a03c2be76627b2cd2a2
Gabriel <gabriel.chu.v@hotmail.com>
2022-02-07 18:27:17 +0000

    InfoBar styling and dynamic presentation of room

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './InfoBar.css';
 
-import closeIcon from '../../icons/closeIcon';
-import onlineIcon from '../../icons/onlineIcon';
+import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from '../../icons/onlineIcon.png';
 
-function Infobar() {
+const Infobar = ({ room }) => (
     <div className="infoBar"> 
         <div className="leftInnerContainer">
             <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-            <h3>roomName</h3>
+            <h3>{room}</h3>
         </div>
         <div className="rightInnerContainer">
             { /* Refreshing in order to get rid of queried strings and to clean up socket */ }
             <a href="/"><img src={closeIcon} alt="close icon" /></a>
         </div>
     </div>
-}
+)
 
 export default Infobar;
